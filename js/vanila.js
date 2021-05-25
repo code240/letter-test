e925f400c2cc8605f0f937bc164974f88f84885c
Vipin <vipinrao03@gmail.com>
2021-05-25 12:19:50 +0530

    updation register

--- js/vanila.js
+++ js/vanila.js
@@ -1,4 +1,18 @@
 setTimeout(()=>{
     document.getElementById("intro").style.display = "none";
     document.getElementById("login").style.display = "block";
-},4000);
+    document.getElementById("register").style.display = "none";
+},100);
+
+const loginform = () =>{
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("register").style.display = "none";
+    document.getElementById("login").style.display = "block";
+}
+
+
+const Registerform = () =>{
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("register").style.display = "block";
+    document.getElementById("login").style.display = "none";
+}
